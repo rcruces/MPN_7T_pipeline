823bf7e5a5e1b7be379a7262a2b2a2cb641d637d
Raúl RC <rcruces@users.noreply.github.com>
2024-11-19 15:13:43 -0500

    Organized main script workflow

--- functions/mpn_dcm2bids.py
+++ functions/mpn_dcm2bids.py
@@ -1,8 +1,50 @@
+"""
+This script converts DICOM files to BIDS format using the MPN 7T workflow.
+
+Modules:
+    os: Provides a way of using operating system dependent functionality.
+    subprocess: Allows you to spawn new processes, connect to their input/output/error pipes, and obtain their return codes.
+    tempfile: Generates temporary files and directories.
+    bids_validator: Validates BIDS datasets.
+    argparse: Parses command-line arguments.
+
+Functions:
+    run_command(command):
+        Executes a shell command and raises an error if the command fails.
+        Args:
+            command (str): The command to be executed.
+        Raises:
+            subprocess.CalledProcessError: If the command execution fails.
+
+    main():
+        Main function that sets up the workflow for converting DICOMs to BIDS format.
+        It creates a temporary directory, runs the necessary scripts, and validates the BIDS output.
+
+Arguments:
+    --dicoms_dir (str): Directory containing DICOM files.
+    --bids_dir (str): Output BIDS directory.
+    --sub (str): Subject ID.
+    --ses (str): Session ID.
+"""
 import os
 import subprocess
 import tempfile
-import shutil
+import argparse
 
+# Arguments
+parser = argparse.ArgumentParser(description='Convert DICOMs to BIDS format.')
+parser.add_argument('--dicoms_dir', required=True, help='Directory containing DICOM files')
+parser.add_argument('--bids_dir', required=True, help='Output BIDS directory')
+parser.add_argument('--sub', required=True, help='Subject ID')
+parser.add_argument('--ses', required=True, help='Session ID')
+
+args = parser.parse_args()
+dicoms_dir = args.dicoms_dir
+bids_dir = args.bids_dir
+sub = args.sub
+ses = args.ses
+
+# Set workflow
 def run_command(command):
     try:
         subprocess.run(command, check=True, shell=True)
@@ -10,6 +52,19 @@ def run_command(command):
         print(f"Error occurred while running command: {command}")
         print(e)
         raise
+
+# Workflow steps
+def sort_dicoms():
+    print("Sorting DICOMs with dcm2Sort...")
+    run_command(f'dcm2Sort.sh {dicoms_dir} {os.path.join(bids_dir, "sorted_dicoms")}')
+
+def sorted2bids():
+    print("Converting sorted DICOMs to BIDS format with dcm2niix sorted2bids...")
+    run_command(f'mpn_sorted2bids.sh -b y -z y -o {bids_dir} -f "%p_%s" {os.path.join(bids_dir, "sorted_dicoms")}')
+
+def validate_bids():
+    print("Validating BIDS output with bids-validator...")
+    run_command(f'deno run --allow-write -ERN jsr:@bids/validator {bids_dir} --ignoreWarnings --outfile {bids_dir}/bids_validator_output.txt')
 
 def main():
     # Create a temporary directory
@@ -19,17 +74,21 @@ def main():
         # Change to the temporary directory
         os.chdir(tmpdirname)
 
-        # Run dcmsort.sh
-        print("Running dcmsort.sh...")
-        run_command('./dcmsort.sh')
+        # Run sort_dicoms
+        print("Running Sorting dicoms ...")
+        sort_dicoms()
 
-        # Run mpN_sorted2bids
-        print("Running mpN_sorted2bids...")
-        run_command('mpN_sorted2bids')
+        # Run sorted2bids
+        print("Running Sorted dicoms to BIDS ...")
+        sorted2bids()
 
-        # Run bids_validator
-        print("Running bids_validator...")
-        run_command('bids_validator')
+        # Run validate_bids
+        print("Running BIDS validator ...")
+        validate_bids()
+
+        # Print validate_bids output
+        with open(os.path.join(bids_dir, 'bids_validator_output.txt'), 'r') as file:
+            print(file.read())
 
         print("Workflow completed successfully.")
 
